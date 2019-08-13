e22672f1c3787c63be0b88a0180f96470144c006
temitayo ogunlolu <temitayo094@gmail.com>
2019-08-13 03:12:56 +0100

    Implementated currency convertion to each country details page

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import CurrencyCoverter from './CurrencyCoverter';
 
+const divStyle = {
+    width: '15rem',
+}
 
-const CountryDetails = ({name, capital, population, flag}) => {
+const CountryDetails = ({name, capital, population, flag, currencies}) => {
+
+    if(!currencies){
+        return (<span>Loading...</span>)
+    }
    
     return (
          <React.Fragment>
@@ -13,19 +20,22 @@ const CountryDetails = ({name, capital, population, flag}) => {
                    <div className="my-3 p-3 bg-white rounded shadow-sm">
                       
                        <div>
-                           <img src={flag} alt={flag} />
+                           <img src={flag} alt={flag} style={divStyle}/>
                             
                            <h2>{name}</h2>
                            <h4>{capital}</h4>
                            <p>{population}</p>
+                           <p>{currencies[0].name}</p>
+                         
+                
                        </div>
                    </div>
                </div>
 
                 <div className="col-md-4">
                       <div className="my-3 p-3 bg-white rounded shadow-sm">
-                           <h3>Convert SEK to local currency</h3>
-                           <CurrencyCoverter />
+                            <h3>Convert EUR to {name}</h3>
+                           <CurrencyCoverter code={currencies[0].code} symbol={currencies[0].symbol} />
                           </div> 
                 </div>
 
--- src/components/CurrencyCoverter.js
+++ src/components/CurrencyCoverter.js
@@ -1,13 +1,86 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const CurrencyCoverter = () => {
-    return (
-        <React.Fragment>
+export default class CurrencyCoverter extends Component {
+
+    constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      fromCurrency: "EUR",
+      toCurrency: props.code,
+      amount: 1,
+      currencies: []
+    };
+  }
+
+  selectHandler = e => {
+    if (e.target.name === "from") {
+      this.setState({ fromCurrency: e.target.value });
+    } else {
+      if (e.target.name === "to") {
+        this.setState({ toCurrency: e.target.value });
+      }
+    }
+  };
+
+  convertHandler = () => {
+    if (this.state.fromCurrency !== this.state.toCurrency) {
+      axios
+        .get(
+          `http://data.fixer.io/api/latest?access_key=0c39153c808f671475fd6b52d5a22a8a&base=${
+            this.state.fromCurrency
+          }&symbols=${this.state.toCurrency}`
+        )
+        .then(response => {
+          const result =
+            this.state.amount * response.data.rates[this.state.toCurrency];
+          this.setState({ result: result.toFixed(5) });
+        })
+        .catch(error => {
+          console.log("Opps", error.message);
+        });
+    } else {
+      this.setState({ result: "You cant convert the same currency!" });
+    }
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://data.fixer.io/api/latest?access_key=0c39153c808f671475fd6b52d5a22a8a")
+      .then(response => {
+        const currencyAr = ["EUR"];
+        for (const key in response.data.rates) {
+          currencyAr.push(key);
+        }
+        this.setState({ currencies: currencyAr });
+      })
+      .catch(err => {
+        console.log("oppps", err);
+      });
+  }
+
+    render() {
+        return (
+            <React.Fragment>
             <div>
-                <input type="text" className="form-control" />
+               
+                <div className="From">
+          <input
+            name="amount"
+            type="text"
+            value={this.state.amount}
+            onChange={event => this.setState({ amount: event.target.value })}
+          />
+         
+           <p>{this.state.fromCurrency}</p>
+         
+           <p>{this.state.toCurrency}</p>
+          <button onClick={this.convertHandler}>Convert</button>
+          {this.state.result && <h3><span>{this.props.symbol} </span>{this.state.result}</h3>}
+        </div>
             </div>
         </React.Fragment>
-    )
+        )
+    }
 }
-
-export default CurrencyCoverter
